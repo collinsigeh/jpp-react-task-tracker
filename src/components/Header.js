8a083d2ad1913d903186065abd45c4245073a9c3
Collins Igeh <igehac@gmail.com>
2022-07-27 23:11:09 +0100

    created the header component

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import PropTypes from "prop-types"
+
+const Header = (props) => {
+  return (
+    <div>
+        <h1>{ props.title }</h1>
+    </div>
+  )
+}
+
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+export default Header
