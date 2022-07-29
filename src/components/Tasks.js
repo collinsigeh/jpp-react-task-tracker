a902c05fb0e26aa9b1799bdea99ce34ce1e0209d
Collins Igeh <igehac@gmail.com>
2022-07-29 13:46:52 +0100

    implemented a conditional notice when no task to display

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -3,9 +3,11 @@ import Task from "./Task";
 const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
-      {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={ onDelete } />
-      ))}
+      {tasks.length > 0
+        ? tasks.map((task) => (
+            <Task key={task.id} task={task} onDelete={onDelete} />
+          ))
+        : "No task to display!"}
     </>
   );
 };
