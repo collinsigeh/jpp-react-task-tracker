0db94de7b4e14d18045e8b5c45d83314d1ca568f
Collins Igeh <igehac@gmail.com>
2022-07-29 16:16:14 +0100

    created a dummy input form for tasks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import AddTask from "./components/AddTask";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 
@@ -44,6 +45,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
     </div>
   );
